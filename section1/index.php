bcd9aa7a3ce7f2d8790fa91165f9e21837c70bfd
unknown <johnxiler@gmail.com>
2024-10-19 15:20:04 +0800

    i learn about variables

--- section1/index.php
+++ section1/index.php
@@ -20,7 +20,14 @@
     // echo "my name is", $name,"\r\n";
     echo nl2br("my name is $name \r\n ");
     echo "I live in The Philippines <br>";
-    echo "I like programming";
+    echo "I like programming <br>";
+    // a variable is a symbol or name that stands for a value
+    //Variable name starts with a letter A-Z a-z, undescore(_), followed by any number of letters, numbers, or undescores.
+    // just use PascalCase
+    // if variablename starts with a number after the dollar sign it is invalid  
+    $price = 45;
+    // $4hash = 453453; invalid variable...
+    echo "can i buy $price";
     ?>
 </body>
 </html>
