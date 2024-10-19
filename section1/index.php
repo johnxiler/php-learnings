e1d134e00c11b18fadf01599275e98ce3737a462
unknown <johnxiler@gmail.com>
2024-10-19 14:39:43 +0800

    I use nl2br function in php it manipulate line breaks on strings

--- section1/index.php
+++ section1/index.php
@@ -10,11 +10,17 @@
     // phpinfo(); 
     $name = "relix";
     echo "Hello ", $name; //echo is for more strings
-    print "\nprint "."this"; // print is for one string it returns one string only unless concatenate strings
+    print "\nprint "."this.... <br>"; // print is for one string it returns one string only unless concatenate strings
     //single line comment
     /*
     multiline comments
     */
+    //line break lesson
+    // lets a function echo nl2br();
+    // echo "my name is", $name,"\r\n";
+    echo nl2br("my name is $name \r\n ");
+    echo "I live in The Philippines <br>";
+    echo "I like programming";
     ?>
 </body>
 </html>
